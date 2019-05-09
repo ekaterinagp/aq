de3f9c50562b56c5f10cbca421fdd2c26eb9a367
ekaterinagp <katya.koriakina@gmail.com>
2019-05-09 09:48:21 +0200

    clicking and svg

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ const section_whatyouget = document.querySelector("#section_whatyouget");
 let section_whatyouget_items = section_whatyouget.querySelectorAll(".item");
 section_whatyouget_items.forEach(item => {
   item.addEventListener("click", () => {
-    clearAllItemsStyle(items, "section_whatyouget");
+    clearAllItemsStyle(section_whatyouget_items, "section_whatyouget");
     console.log({ item });
     applyStyle(item);
     changeImage(item, section_whatyouget);
@@ -43,3 +43,62 @@ function resizeText(multiplier, p) {
   }
   p.style.fontSize = parseFloat(p.style.fontSize) + multiplier * 0.2 + "em";
 }
+
+const changeText = (item, section) => {
+  let textDiv = section.querySelector("p");
+  let title = section.querySelector("h2");
+
+  if (item.id === "section_platform_item_1")
+    textDiv.textContent =
+      "Beautiful text about overview overviewnostrum repellat harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim.";
+  if (item.id == "section_platform_item_2")
+    textDiv.textContent =
+      "Matching matching matching repellat harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  if (item.id == "section_platform_item_3")
+    textDiv.textContent =
+      "Communication is the best in the world repellat harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  if (item.id == "section_platform_item_4")
+    textDiv.textContent =
+      "Deal flow flows the best repellat harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  if (item.id == "section_why_item_1") {
+    title.textContent = "Save time";
+    textDiv.textContent =
+      "Save all your time in the world!  harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  }
+
+  if (item.id == "section_why_item_2") {
+    title.textContent = "Hassle free";
+    textDiv.textContent =
+      "Never hassle again! harum iste magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  }
+
+  if (item.id == "section_why_item_3") {
+    title.textContent = "Better process";
+    textDiv.textContent =
+      "Soooo better process!! magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  }
+
+  if (item.id == "section_why_item_4") {
+    title.textContent = "No obligation";
+    textDiv.textContent =
+      "Absolutely ni obligation!! magni numquam, quisquam non soluta enim.Lorem ipsum dolor sit amet consectetur adipisicing elit.Facere sunt laborum magnam quia! Vel ipsum, nobis eveniet quod animi eum nostrum repellat harum iste magni numquam, quisquam non soluta enim";
+  }
+};
+
+const section_platform = document.querySelector("#section_platform");
+let section_platform_items = section_platform.querySelectorAll(".icon");
+section_platform_items.forEach(item => {
+  item.addEventListener("click", () => {
+    console.log({ item });
+    changeText(item, section_platform);
+  });
+});
+
+const section_why = document.querySelector("#section_why");
+let section_why_items = section_why.querySelectorAll(".items");
+section_why_items.forEach(item => {
+  item.addEventListener("click", () => {
+    console.log({ item });
+    changeText(item, section_why);
+  });
+});

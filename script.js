a18f8ea911cb26e5921d8474f56ab3cdeb3fe855
unknown <elberoest@gmail.com>
2019-05-14 09:20:03 +0200

    faq and prices

--- script.js
+++ script.js
@@ -140,13 +140,14 @@ section_platform_items.forEach(item => {
     changeText(item, section_platform);
   });
 });
-const arch_projects= document.querySelector("#arch_projects");
-let section_arch = arch_projects.querySelectorAll(".items");
-section_arch.forEach(item=>{
-  item.addEventListener("click", () => {
-    console.log(item);
-  })
-})
+// let arch_projects = document.querySelector("#arch_projects");
+// let section_arch = arch_projects.querySelectorAll(".items");
+// section_arch.forEach(item=>{
+//   item.addEventListener("click", () => {
+//     console.log("item", item);
+//   })
+// })
+
 const section_why = document.querySelector("#section_why");
 let section_why_items = section_why.querySelectorAll(".items");
 section_why_items.forEach(item => {

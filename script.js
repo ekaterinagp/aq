9a4ca61ec6dc7ed8764ad76f2abd1bf37d181d53
unknown <elberoest@gmail.com>
2019-05-17 20:08:04 +0200

    some of the images

--- script.js
+++ script.js
@@ -786,15 +786,15 @@ const changeImage = (item, section) => {
   console.log({ "item.id": item.id });
 
   if (item.id === "section_whatyouget_item_1")
-    img.setAttribute("src", "img/laptop.png");
+    img.setAttribute("src", "img/formfirst.png");
   if (item.id === "section_whatyouget_item_2")
-    img.setAttribute("src", "img/platform.png");
+    img.setAttribute("src", "img/platform_home.png");
   if (item.id === "section_whatyouget_item_3")
-    img.setAttribute("src", "img/house.png");
-  if (item.id === "resedential") img.setAttribute("src", "img/house.png");
-  if (item.id === "business") img.setAttribute("src", "img/building.png");
-  if (item.id === "education") img.setAttribute("src", "img/office.png");
-  if (item.id === "aestetic") img.setAttribute("src", "img/design.png");
+    img.setAttribute("src", "img/laptop_placeholder.png");
+  if (item.id === "resedential") img.setAttribute("src", "img/resihouse.png");
+  if (item.id === "business") img.setAttribute("src", "img/office.png");
+  if (item.id === "education") img.setAttribute("src", "img/school.png");
+  if (item.id === "aestetic") img.setAttribute("src", "img/gazebo.png");
 };
 
 const applyFill = (item, section) => {

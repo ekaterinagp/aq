6d2941cd26bb67ca5c95a18c36dc7edf5f7c3e5d
ekaterinagp <katya.koriakina@gmail.com>
2019-05-23 11:40:44 +0200

    wireframes new approach started

--- wireframe.js
+++ wireframe.js
@@ -1,59 +1,31 @@
 "use strict";
 
-window.addEventListener("load", () => {
-  init();
-});
-
-function fetchParts() {
-  let endpoint = "https://architecturequote.com/wp-json/wp/v2/wireframe1";
-  return new Promise((resolve, reject) => {
-    fetch(endpoint)
-      .then(res => res.json())
-      .then(function(parts) {
-        resolve(parts);
-      });
-  });
+//creates a simple above-the-fold with a background image, h1 and subtitle from WP
+function createAboveTheFold(parts, img, i) {
+  let abovetheFoldHolder = document.createElement("div");
+  abovetheFoldHolder.setAttribute("id", "hero-img");
+  abovetheFoldHolder.setAttribute("class", "hero");
+  abovetheFoldHolder.style.backgroundImage =
+    "url(" + img.media_details.sizes.large.source_url + ")";
+  let divForText = document.createElement("div");
+  divForText.setAttribute("class", "hero_text");
+  let h1 = document.createElement("h1");
+  h1.innerHTML = parts[i].title.rendered;
+  let p = document.createElement("p");
+  p.setAttribute("class", "sub_title");
+  p.innerHTML = parts[i].excerpt.rendered;
+  let button = createButtonForForm();
+  divForText.append(h1, p, button);
+  abovetheFoldHolder.append(divForText);
+  document.querySelector(".wrapper").append(abovetheFoldHolder);
 }
 
-async function insertPartsToDOM(parts) {
-  const backgroundIMG = await fetch(
-    parts[2]._links["wp:featuredmedia"][0].href
-  ).then(res => res.json());
-  console.log({ backgroundIMG });
-  document.querySelector("#hero-img").style.backgroundImage =
-    "url(" + backgroundIMG.media_details.sizes.large.source_url + ")";
-  document.querySelector("#hero-img").querySelector("h1").innerHTML =
-    parts[2].title.rendered;
-  document.querySelector("#hero-img").querySelector("p").innerHTML =
-    parts[2].excerpt.rendered;
+function createButtonForForm() {
+  let button = document.createElement("button");
+  let classesToAdd = ["orange_button", "freeEst"];
 
-  document
-    .querySelector("#wireframe_description")
-    .querySelector("h1").textContent = parts[1].title.rendered;
-  document
-    .querySelector("#wireframe_description")
-    .querySelector("p").innerHTML = parts[1].content.rendered;
+  button.classList.add(...classesToAdd);
 
-  document.querySelector("#benefits").querySelector("h1").innerHTML =
-    parts[0].title.rendered;
-  document.querySelector("#benefits_list").querySelector("p").innerHTML =
-    parts[0].content.rendered;
-}
-
-let sequenceNumber = 1;
-let slideShow = document.querySelector("#slide");
-
-function changeSrc() {
-  slideShow.src = "cabin/slideshow" + sequenceNumber + ".jpg";
-  sequenceNumber++;
-  if (sequenceNumber > 6) {
-    sequenceNumber = 0;
-  }
-}
-
-async function init() {
-  const wireframeParts = await fetchParts();
-  console.log({ wireframeParts });
-  insertPartsToDOM(wireframeParts);
-  setInterval(changeSrc, 4000);
+  button.textContent = "Get your free estimate";
+  return button;
 }
